40eb7f8654c71ab4574acb199f570ee657d01393
iamayaz <imphpdevelope@gmail.com>
2017-05-09 16:17:05 +0400

    Header Layout
    I added header layout

--- my-app/src/header.js
+++ my-app/src/header.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from './logo.svg';
 class Header extends Component {
-	  
   render() {
-
 	const 	title = 'Hello React! Header';
 	const  	time = new Date().toLocaleTimeString();
 	const 	element = (
 		<header>
+
+			  <img src={logo} className="App-logo" alt="logo" />
+			 
 			<h1>{title}</h1>
 			<h2>It is {time}.</h2>
+			<ul>
+				<li>
+					<Link to="Body"></Link>
+					<a href="Body">Body</a>
+				</li>
+				<li>
+					<Link to="User"></Link>
+					<a href="User">User</a>
+				</li>
+			</ul>
 		</header>
 	);
     return (
@@ -17,5 +30,4 @@ class Header extends Component {
   }
 
 }
-
 export default Header;
