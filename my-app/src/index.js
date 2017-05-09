b4e2ea857b1b34a9d585ce602a75d173defb302b
iamayaz <imphpdevelope@gmail.com>
2017-05-09 16:17:48 +0400

    Remove the SetInterval
    Remove the SetInterval from index.js file.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
-
-function RenderDOM(){
+ReactDOM.render(
+	<App />,
+	document.getElementById('first-app-root')
+);
+/* function RenderDOM(){
 	ReactDOM.render(
 		<App />,
 		document.getElementById('first-app-root')
 	);
 }
-
-setInterval(RenderDOM, 1000);
+setInterval(RenderDOM, 1000); */
